82ba89caee98e8d502164d0346cacb6fa30133bc
cjrace <cjrace>
2025-03-25 00:18:57 +0000

    add rolling averages (and fallback mirror)

--- data-updates/notebooks/app_ees_service_summary.r
+++ data-updates/notebooks/app_ees_service_summary.r
@@ -2,7 +2,7 @@
 # DBTITLE 1,Load dependencies
 source("utils.R")
 
-packages <- c("sparklyr", "DBI", "dplyr", "testthat", "arrow")
+packages <- c("sparklyr", "DBI", "dplyr", "testthat", "arrow" , "TTR")
 
 install_if_needed(packages)
 lapply(packages, library, character.only = TRUE)
@@ -24,19 +24,28 @@ sc <- spark_connect(method = "databricks")
 # DBTITLE 1,Join together and check table integrity
 aggregated_data <- sparklyr::sdf_sql(
   sc, paste("
-    SELECT date, pageviews as screenPageViews, sessions, avg_session_duration as averageSessionDuration FROM", ua_service_table_name, "
+    SELECT date, pageviews, sessions, avg_session_duration as averageSessionDuration FROM", ua_service_table_name, "
       UNION ALL
-    SELECT date, screenPageViews, sessions, averageSessionDuration FROM", ga4_service_table_name, "
+    SELECT date, screenPageViews as pageviews, sessions, averageSessionDuration FROM", ga4_service_table_name, "
     ORDER BY date DESC;
   ")
 ) %>% collect()
+
+# Create rolling averages (simple moving average)
+aggregated_data <- aggregated_data %>%
+  arrange(date) %>%
+  mutate(
+    pageviews_avg7 = TTR::SMA(pageviews, n = 7),
+    sessions_avg7 = TTR::SMA(sessions, n = 7)    
+  )
 
 test_that("No duplicate rows", {
   expect_true(nrow(aggregated_data) == nrow(dplyr::distinct(aggregated_data)))
 })
 
 test_that("Data has no missing values", {
-  expect_false(any(is.na(aggregated_data)))
+  # We expect the rolling average cols to have some missing values so ignoring for this
+  expect_false(any(is.na(aggregated_data |> select(-c(pageviews_avg7, sessions_avg7))))) 
 })
 
 dates <- create_dates(max(aggregated_data$date))
